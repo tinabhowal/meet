0e6c9a4e5bfb6ee7a0184c413b317702e9fd102a
tinabhowal <117632464+tinabhowal@users.noreply.github.com>
2023-04-12 09:40:28 +0200

    Task 4.9 pasrt 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
+
 import ReactDOM from 'react-dom';
 import * as atatus from 'atatus-spa';
 import './index.css';
@@ -8,12 +8,7 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 atatus.config('0a92904c5c7c4fd48123888288f69361').install();
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+
 const root = document.getElementById('root');
 ReactDOM.render(
   <React.StrictMode>
@@ -25,7 +20,12 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+
+//serviceWorkerRegistration.unregister();
+
+serviceWorkerRegistration.register();
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
