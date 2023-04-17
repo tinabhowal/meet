f0824fde2cacb3551aecf2c118ea65752cf7616c
tinabhowal <117632464+tinabhowal@users.noreply.github.com>
2023-04-17 20:20:56 +0200

    Task 4.10 part2-Applying 2nd chart-pie chart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,56 +1,5 @@
 
 
-// import React, { useEffect, useState, useCallback } from 'react';
-// import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from 'recharts';
-
-// const EventGenre = ({ events }) => {
-//     const [data, setData] = useState([]);
-    
-
-//     const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
-
-    
-
-//     const getData = useCallback(() => {
-//         const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-//         const data = genres.map((genre)=>{
-//             const value = events.filter((event) => event.summary.split('').includes(genre)).length
-            
-//             return { name: genre, value };
-//         });
-//         return data.filter((events) => events.value !== 0);
-//     }, [events]);
-    
-//     useEffect(() => {
-//         setData(getData());
-//     }, [getData]);
-    
-
-//     return (
-//         <ResponsiveContainer height={400} >
-//             <PieChart width={400} height={400}>
-//                 <Pie
-//                     data={data}
-//                     cx="50%"
-//                     cy="50%"
-//                     labelLine={false}
-//                     outerRadius={120}
-//                     fill="#8884d8"
-//                     dataKey="value"
-//                     label={({ percent }) => `${(percent * 100).toFixed(0)}%`}>
-//                     {data.map((entry, index) => (
-//                         <Cell key={`cell-${index}`} fill={colors[index]} />
-//                     ))
-//                     }
-//                 </Pie>
-//                 <Legend verticalAlign="bottom" height={50} />
-//             </PieChart>
-//         </ResponsiveContainer>
-//     );
-// }
-
-// export default EventGenre;
-
 import React, { useEffect, useState } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from 'recharts';
 
@@ -61,12 +10,12 @@ const EventGenre = ({ events }) => {
     useEffect(() => {
         const getData = () => {
             const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-            // Compute the value (i.e., count) for each genre based on the events that match its regular expression
+           
             const data = genres.map((genre) => {
                 const value = events.filter(event => event.summary.split(' ').includes(genre)).length
                 return { name: genre, value };
             })
-            // Return the data array
+          
             return data;
         };
         setData(() => getData());
