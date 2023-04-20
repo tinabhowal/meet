6da22bbf64534ee73b48ad0d6cc1bf252dae232f
tinabhowal <117632464+tinabhowal@users.noreply.github.com>
2023-04-20 16:42:01 +0200

    Adding Bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import ReactDOM from 'react-dom';
 import * as atatus from 'atatus-spa';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
