170f2ecf7e2ec68141c4a6264eed6366eb278ba2
tinabhowal <117632464+tinabhowal@users.noreply.github.com>
2023-04-20 21:09:51 +0200

    Adding genre image

--- src/Event.js
+++ src/Event.js
@@ -22,29 +22,52 @@ class Event extends Component {
       const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
       const isGenre = genres.some(genre => event.summary.includes(genre));
       
+      // const getGenreImage = (genre) => {
+      //   console.log('getGenreImage called with genre:', genre);
+      //   switch(genre) {
+      //     case 'React':
+      //       return <img src={react} alt="React" width="50" />;
+      //     case 'JavaScript':
+      //       return <img src={javascript} alt="JavaScript" width="50" />;
+      //     case 'Node':
+      //       return <img src={nodejs} alt="Node" width="50" />;
+      //     case 'jQuery':
+      //       return <img src={jquery} alt="jQuery" width="50" />;
+      //     case 'AngularJS':
+      //       return <img src={angularjs} alt="AngularJS" width="50" />;
+      //     default:
+      //       return null;
+      //   }
+      // }
+        
       const getGenreImage = (genre) => {
-        console.log('getGenreImage called with genre:', genre);
         switch(genre) {
           case 'React':
-            return <img src={react} alt="React" width="50" />;
+            const ReactIcon = require('./react.svg').default;
+            return <img src={ReactIcon} alt="React" width="50" />;
           case 'JavaScript':
-            return <img src={javascript} alt="JavaScript" width="50" />;
+            const JavaScriptIcon = require('./javascript.svg').default;
+            return <img src={JavaScriptIcon} alt="JavaScript" width="50" />;
           case 'Node':
-            return <img src={nodejs} alt="Node" width="50" />;
+            const NodeIcon = require('./nodejs.svg').default;
+            return <img src={NodeIcon} alt="Node" width="50" />;
           case 'jQuery':
-            return <img src={jquery} alt="jQuery" width="50" />;
+            const jQueryIcon = require('./jquery.svg').default;
+            return <img src={jQueryIcon} alt="jQuery" width="50" />;
           case 'AngularJS':
-            return <img src={angularjs} alt="AngularJS" width="50" />;
+            const AngularJSIcon = require('./angularjs.svg').default;
+            return <img src={AngularJSIcon} alt="AngularJS" width="50" />;
           default:
             return null;
         }
       }
-        
+      
+
     return (
     <Card key={event.id} className="my-3">
         <Card.Header className="bg-primary text-white">{event.summary}</Card.Header>
         <Card.Body>
-          {isGenre && (getGenreImage(event.summary))}
+          {isGenre && getGenreImage(event.summary)}
           <Card.Text>
             <strong>Start:</strong> {event.start.dateTime}
             <br />
