381d915a5c68158f383285a65453fa57c40d1f10
tinabhowal <117632464+tinabhowal@users.noreply.github.com>
2023-04-21 08:17:27 +0200

    Adding genre image

--- src/Event.js
+++ src/Event.js
@@ -1,3 +1,102 @@
+// import React, { Component } from "react";
+// import { Card,Button } from 'react-bootstrap';
+// import angularjs from './angularjs.svg';
+// import javascript from './javascript.svg';
+// import nodejs from './nodejs.svg';
+// import jquery from './jquery.svg';
+// import react from './react.svg';
+
+// class Event extends Component {
+//   state = {
+//     showDetails: false,
+//   };
+
+//   handleShowDetailsClick = () => {
+//     this.setState({ showDetails: !this.state.showDetails });
+//   };
+  
+
+//   render() {
+//     const {event} = this.props;
+    
+//       const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+//       const isGenre = genres.some(genre => event.summary.includes(genre));
+      
+//       // const getGenreImage = (genre) => {
+//       //   console.log('getGenreImage called with genre:', genre);
+//       //   switch(genre) {
+//       //     case 'React':
+//       //       return <img src={react} alt="React" width="50" />;
+//       //     case 'JavaScript':
+//       //       return <img src={javascript} alt="JavaScript" width="50" />;
+//       //     case 'Node':
+//       //       return <img src={nodejs} alt="Node" width="50" />;
+//       //     case 'jQuery':
+//       //       return <img src={jquery} alt="jQuery" width="50" />;
+//       //     case 'AngularJS':
+//       //       return <img src={angularjs} alt="AngularJS" width="50" />;
+//       //     default:
+//       //       return null;
+//       //   }
+//       // }
+        
+//       const getGenreImage = (genre) => {
+//         console.log('getGenreImage called with genre:', genre);
+//         switch(genre) {
+//           case 'React':
+//             const ReactIcon = require('./react.svg').default;
+//             console.log('reacticon:', ReactIcon);
+//             return <img src={ReactIcon} alt="React" width="50" />;
+//           case 'JavaScript':
+//             const JavaScriptIcon = require('./javascript.svg').default;
+//             return <img src={JavaScriptIcon} alt="JavaScript" width="50" />;
+//           case 'Node':
+//             const NodeIcon = require('./nodejs.svg').default;
+//             return <img src={NodeIcon} alt="Node" width="50" />;
+//           case 'jQuery':
+//             const jQueryIcon = require('./jquery.svg').default;
+//             return <img src={jQueryIcon} alt="jQuery" width="50" />;
+//           case 'AngularJS':
+//             const AngularJSIcon = require('./angularjs.svg').default;
+//             return <img src={AngularJSIcon} alt="AngularJS" width="50" />;
+//           default:
+//             return null;
+//         }
+//       }
+      
+
+//     return (
+//     <Card key={event.id} className="my-3">
+//         <Card.Header className="bg-primary text-white">{event.summary}</Card.Header>
+//         <Card.Body>
+//           {isGenre && getGenreImage(event.summary)}
+//           <Card.Text>
+//             <strong>Start:</strong> {event.start.dateTime}
+//             <br />
+//             <strong>Location:</strong> {event.location}
+//           </Card.Text>
+//           <Button
+//             className="btn btn-primary"
+//             onClick={this.handleShowDetailsClick}
+//           >
+//             {this.state.showDetails ? "Hide details" : "Show Details"}
+//           </Button>
+//           {this.state.showDetails && (
+//             <Card.Text>
+//               <h4 className="about-event">About Event</h4>
+//               <a href={event.htmlLink} className="about-event-link">
+//                 See details in Google Calendar
+//               </a>
+//               <p className="event-description">{event.description}</p>
+//             </Card.Text>
+//             )}
+//             </Card.Body>
+//           </Card>
+//     );
+//   }
+// }
+// export default Event;
+
 import React, { Component } from "react";
 import { Card,Button } from 'react-bootstrap';
 import angularjs from './angularjs.svg';
@@ -9,67 +108,65 @@ import react from './react.svg';
 class Event extends Component {
   state = {
     showDetails: false,
+    imagesLoaded: false // new state variable to indicate when images are ready
   };
+
+  // new function to preload images
+  preloadImages = () => {
+    const images = [angularjs, javascript, nodejs, jquery, react];
+    const promises = [];
+    images.forEach(image => {
+      promises.push(
+        new Promise(resolve => {
+          const img = new Image();
+          img.onload = () => resolve();
+          img.src = image;
+        })
+      );
+    });
+    Promise.all(promises).then(() => {
+      this.setState({ imagesLoaded: true });
+    });
+  }
+
+  componentDidMount() {
+    this.preloadImages();
+  }
 
   handleShowDetailsClick = () => {
     this.setState({ showDetails: !this.state.showDetails });
   };
   
-
+  // updated getGenreImage function to use the imported images directly
+  getGenreImage = (genre) => {
+    switch(genre) {
+      case 'React':
+        return <img src={react} alt="React" width="50" />;
+      case 'JavaScript':
+        return <img src={javascript} alt="JavaScript" width="50" />;
+      case 'Node':
+        return <img src={nodejs} alt="Node" width="50" />;
+      case 'jQuery':
+        return <img src={jquery} alt="jQuery" width="50" />;
+      case 'AngularJS':
+        return <img src={angularjs} alt="AngularJS" width="50" />;
+      default:
+        return null;
+    }
+  }
+        
   render() {
     const {event} = this.props;
-    
-      const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-      const isGenre = genres.some(genre => event.summary.includes(genre));
-      
-      // const getGenreImage = (genre) => {
-      //   console.log('getGenreImage called with genre:', genre);
-      //   switch(genre) {
-      //     case 'React':
-      //       return <img src={react} alt="React" width="50" />;
-      //     case 'JavaScript':
-      //       return <img src={javascript} alt="JavaScript" width="50" />;
-      //     case 'Node':
-      //       return <img src={nodejs} alt="Node" width="50" />;
-      //     case 'jQuery':
-      //       return <img src={jquery} alt="jQuery" width="50" />;
-      //     case 'AngularJS':
-      //       return <img src={angularjs} alt="AngularJS" width="50" />;
-      //     default:
-      //       return null;
-      //   }
-      // }
-        
-      const getGenreImage = (genre) => {
-        console.log('getGenreImage called with genre:', genre);
-        switch(genre) {
-          case 'React':
-            const ReactIcon = require('./react.svg').default;
-            console.log('reacticon:', ReactIcon);
-            return <img src={ReactIcon} alt="React" width="50" />;
-          case 'JavaScript':
-            const JavaScriptIcon = require('./javascript.svg').default;
-            return <img src={JavaScriptIcon} alt="JavaScript" width="50" />;
-          case 'Node':
-            const NodeIcon = require('./nodejs.svg').default;
-            return <img src={NodeIcon} alt="Node" width="50" />;
-          case 'jQuery':
-            const jQueryIcon = require('./jquery.svg').default;
-            return <img src={jQueryIcon} alt="jQuery" width="50" />;
-          case 'AngularJS':
-            const AngularJSIcon = require('./angularjs.svg').default;
-            return <img src={AngularJSIcon} alt="AngularJS" width="50" />;
-          default:
-            return null;
-        }
-      }
-      
+    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+    const isGenre = genres.some(genre => event.summary.includes(genre));
 
     return (
-    <Card key={event.id} className="my-3">
+      // only render the component if images are ready
+      this.state.imagesLoaded &&
+      <Card key={event.id} className="my-3">
         <Card.Header className="bg-primary text-white">{event.summary}</Card.Header>
         <Card.Body>
-          {isGenre && getGenreImage(event.summary)}
+          {isGenre && this.getGenreImage(event.summary)}
           <Card.Text>
             <strong>Start:</strong> {event.start.dateTime}
             <br />
@@ -89,9 +186,9 @@ class Event extends Component {
               </a>
               <p className="event-description">{event.description}</p>
             </Card.Text>
-            )}
-            </Card.Body>
-          </Card>
+          )}
+        </Card.Body>
+      </Card>
     );
   }
 }
